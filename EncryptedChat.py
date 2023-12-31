a733ad8ec9476613a583b137530bae439eaaeefd
alielabasery <125434943+alielabasery@users.noreply.github.com>
2023-12-30 22:38:55 -0700

    Update EncryptedChat.py
    Added a close connection function to close the socket connection when the threads finish. 
    Improved the error handling in the send and receive functions and added print statements.

--- EncryptedChat.py
+++ EncryptedChat.py
@@ -20,14 +20,8 @@ try:
     rsa = __import__('rsa')
 except ImportError:
     print("Installing the 'rsa' package...")
-    
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "rsa"])
-        rsa = __import__('rsa')
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing 'rsa': {e}")
-        sys.exit(1)
 
+    rsa = install_rsa_package()
 
 def generate_key_pair():
     return rsa.newkeys(1024)
@@ -42,6 +36,7 @@ def setup_server(ip_address):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip_address, 9999))
     server.listen()
+    print(f"Server listening on {ip_address}:9999")
     client, _ = server.accept()
     client.send(public_key.save_pkcs1("PEM"))
     public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
@@ -50,6 +45,7 @@ def setup_server(ip_address):
 def setup_client(ip_address):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip_address, 9999))
+    print(f"Connected to {ip_address}:9999")
     public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
     client.send(public_key.save_pkcs1("PEM"))
     return client, public_partner
@@ -63,6 +59,7 @@ def send_message(c, public_partner):
             c.send(rsa.encrypt(message.encode(), public_partner))
         except Exception as e:
             print(f"Error sending message: {e}")
+            break
 
 def receive_message(c, private_key):
     while True:
@@ -73,6 +70,14 @@ def receive_message(c, private_key):
                 break
         except Exception as e:
             print(f"Error receiving message: {e}")
+            break
+
+def close_connection(c):
+    try:
+        c.shutdown(socket.SHUT_RDWR)
+        c.close()
+    except Exception as e:
+        print(f"Error closing connection: {e}")
 
 if __name__ == "__main__":
     public_key, private_key = generate_key_pair()
@@ -90,3 +95,11 @@ if __name__ == "__main__":
 
     threading.Thread(target=send_message, args=(client, public_partner)).start()
     threading.Thread(target=receive_message, args=(client, private_key)).start()
+
+    # Wait for the threads to finish
+    for thread in threading.enumerate():
+        if thread != threading.current_thread():
+            thread.join()
+
+    # Close the connection when the threads finish
+    close_connection(client)
